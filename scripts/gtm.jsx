05e6061a0da5b6d792d7922711eaf6305633ce0f
RicSala <ricsala86@gmail.com>
2023-09-27 14:04:44 +0200

    removing pageview events from gtm component

--- scripts/gtm.jsx
+++ scripts/gtm.jsx
@@ -1,47 +1,46 @@
-"use client"
+'use client';
 
-import { GTM_ID, pageview } from "@/lib/gtm"
-import { usePathname, useSearchParams } from "next/navigation"
-import Script from "next/script"
-import { useEffect } from "react"
+import { GTM_ID } from '@/lib/gtm';
+import { usePathname, useSearchParams } from 'next/navigation';
+import Script from 'next/script';
 
 export default function TagManager() {
-    const pathname = usePathname()
-    const searchParams = useSearchParams()
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
-    useEffect(() => {
-        if (pathname) {
-            pageview(pathname)
-        }
-    }, [pathname, searchParams])
+  // useEffect(() => {
+  //     if (pathname) {
+  //         pageview(pathname)
+  //     }
+  // }, [pathname, searchParams])
 
-    if (process.env.NODE_ENV !== "production") {
-        return null
-    }
+  if (process.env.NODE_ENV !== 'production') {
+    return null;
+  }
 
-    return (
-        <>
-            <noscript>
-                <iframe
-                    src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-                    height="0"
-                    width="0"
-                    style={{ display: "none", visibility: "hidden" }}
-                />
-            </noscript>
-            <Script
-                id="gtm-script"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
+  return (
+    <>
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+          height='0'
+          width='0'
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
+      <Script
+        id='gtm-script'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer', '${GTM_ID}');
   `,
-                }}
-            />
-        </>
-    )
+        }}
+      />
+    </>
+  );
 }

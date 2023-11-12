e3f521eec35a2b6c90466aeefb2bbf20a062dd31
RicSala <ricsala86@gmail.com>
2023-11-12 18:52:56 +0100

    refactor

--- components/display-text.jsx
+++ components/display-text.jsx
@@ -4,10 +4,10 @@ export const DisplayText = ({ text, maxLength = 300 }) => {
   let formattedText;
   if (maxLength)
     formattedText =
-      text.length > maxLength ? text.substring(0, maxLength) + "..." : text;
+      text?.length > maxLength ? text.substring(0, maxLength) + "..." : text;
 
   const formatText = (inputText) => {
-    return inputText.split("\n").map((line, index) => (
+    return inputText?.split("\n").map((line, index) => (
       <React.Fragment key={index}>
         {line}
         <br />

5eb313c0bb67e5994a1dbf2c758d2a071ef642a2
RicSala <ricsala86@gmail.com>
2023-11-12 18:52:21 +0100

    ref

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,7 @@ import Feedback from "@/components/feedback";
 import SupportButton from "@/components/support-button";
 import { GlobalErrorHandler } from "@/errors/global-error-handler";
 import { ThemeProvider } from "@/providers/theme-provider";
+import { TattooDetailsModal } from "./(site)/(public)/tatuajes/(grids)/@modal/(.)detalle/[tattooId]/tattoo-details-modal";
 
 const inter = Inter({ subsets: ["latin"] });
 

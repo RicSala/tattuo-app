12ea6cfe88cde76b7a1f66ba03ee8a41bd45a9d5
RicSala <ricsala86@gmail.com>
2023-11-16 12:24:57 +0100

    fix mailgun

--- app/api/mg/route.js
+++ app/api/mg/route.js
@@ -1,7 +1,16 @@
+import { sendEmail } from "@/lib/mailgun";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
   const body = await request.json();
+
+  await sendEmail(
+    "ricardo@grouz.io",
+    "New Feedback received",
+    `Rating: ${body.value}<br>Message: ${body.message}<br>`,
+    `<strong>Rating</strong>: ${body.value}<br><strong>Message</strong>: ${body.message}<br>`,
+    "ricardo@ricardo.com",
+  );
 
   console.log("received");
   console.log(body);

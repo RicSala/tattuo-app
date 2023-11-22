f2324cb68c7ee2267345b4f44614919a9140535f
RicSala <ricsala86@gmail.com>
2023-11-22 13:22:38 +0100

    Stop tracking folder

--- app/api/superadmin/route.js
+++
@@ -1,8 +0,0 @@
-// import { getData } from "@/app/scr/scraper";
-// import { NextResponse } from "next/server";
-
-// export async function GET(req) {
-//   const pages = await getData();
-
-//   return NextResponse.json({ message: "ok" }, { status: 201 });
-// }

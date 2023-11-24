014dc66496e8cc85f24d4b23bd1858e585d03260
RicSala <ricsala86@gmail.com>
2023-11-24 16:57:45 +0100

    prefetch nav menu

--- components/navbar/nav-menu.jsx
+++ components/navbar/nav-menu.jsx
@@ -1,24 +1,21 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import Link from "next/link"
+import * as React from "react";
+import Link from "next/link";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 import {
-    NavigationMenu,
-    NavigationMenuLink,
-    NavigationMenuList,
-    navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu"
+  NavigationMenu,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 
-
-export function NavMenu({
-    currentUser
-}) {
-    return (
-        <NavigationMenu>
-            <NavigationMenuList>
-                {/* <NavigationMenuItem>
+export function NavMenu({ currentUser }) {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        {/* <NavigationMenuItem>
                     <NavigationMenuTrigger>Tatuajes</NavigationMenuTrigger>
                     <NavigationMenuContent>
                         <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
@@ -68,48 +65,49 @@ export function NavMenu({
                     </NavigationMenuContent>
                 </NavigationMenuItem> */}
 
-                <Link href="/tatuadores" legacyBehavior passHref>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                        Encuentra Tatuador
-                    </NavigationMenuLink>
-                </Link>
+        <Link href="/tatuadores" legacyBehavior passHref>
+          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            Encuentra Tatuador
+          </NavigationMenuLink>
+        </Link>
 
-                <Link href="/tatuajes" legacyBehavior passHref>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                        Descubre Tatuajes
-                    </NavigationMenuLink>
-                </Link>
+        <Link href="/tatuajes" legacyBehavior passHref>
+          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            Descubre Tatuajes
+          </NavigationMenuLink>
+        </Link>
 
-
-                <Link href="/blog" legacyBehavior passHref>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                        Consejos
-                    </NavigationMenuLink>
-                </Link>
-            </NavigationMenuList>
-        </NavigationMenu>
-    )
+        <Link href="/blog" legacyBehavior passHref>
+          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            Consejos
+          </NavigationMenuLink>
+        </Link>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
 }
 
-const ListItem = React.forwardRef(({ className, title, children, ...props }, ref) => {
+const ListItem = React.forwardRef(
+  ({ className, title, children, ...props }, ref) => {
     return (
-        <li>
-            <NavigationMenuLink asChild>
-                <a
-                    ref={ref}
-                    className={cn(
-                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-                        className
-                    )}
-                    {...props}
-                >
-                    <div className="text-sm font-medium leading-none">{title}</div>
-                    <p className="text-sm leading-snug line-clamp-2 text-muted-foreground">
-                        {children}
-                    </p>
-                </a>
-            </NavigationMenuLink>
-        </li>
-    )
-})
-ListItem.displayName = "ListItem"
+      <li>
+        <NavigationMenuLink asChild>
+          <a
+            ref={ref}
+            className={cn(
+              "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+              className,
+            )}
+            {...props}
+          >
+            <div className="text-sm font-medium leading-none">{title}</div>
+            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+              {children}
+            </p>
+          </a>
+        </NavigationMenuLink>
+      </li>
+    );
+  },
+);
+ListItem.displayName = "ListItem";
